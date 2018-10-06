5e903688222bf263391c362425ad48d53d5d81e7
Viper-PC\Viper <kreppsjs@gmail.com>
2018-10-06 11:02:26 -0400

    Finished testing for ch1 prob1 solutions

--- CrackingTheCodingInterview/ch1_prob1.cpp
+++ CrackingTheCodingInterview/ch1_prob1.cpp
@@ -1,31 +1,98 @@
 #include <ch1_prob1.h>
 #include <unordered_set>
 #include <string>
+#include <array>
+#include <bitset>
 #include <assert.h>
+#include <Windows.h>
 
-bool is_unique(std::string const& arg)
+
+bool is_unique_bool_array(std::string const& input_string)
+{
+    //std::array<bool> is NOT a bitset. We'll do that after.
+    std::array<bool, 256> ascii_check_map = { false };
+    for (char const& i : input_string)
+    {
+        if (ascii_check_map[i])
+            return false;
+        else
+            ascii_check_map[i] = true;
+    }
+    return true;
+}
+
+
+bool is_unique_bitset(std::string const& input_string)
+{
+    std::bitset<256> bitset_check_map;
+    for (char const& i : input_string)
+    {
+        if (bitset_check_map[i])
+            return false;
+        else
+            bitset_check_map[i] = true;
+    }
+    return true;
+}
+
+//This implementation will have to just store the string and loop over the substrings
+//repeatedly as a naive solution. O(n^2)
+bool is_unique_no_data_structures(std::string const& input_string)
+{
+    for (size_t i = 0; i < input_string.size(); ++i)
+    {
+        for (size_t j = 0; j < input_string.size(); ++j)
+        {
+            if (i == j)
+                continue;
+            if (input_string[i] == input_string[j])
+                return false;
+        }
+    }
+    return true;
+}
+
+//RVO: ZS said is related to single-return path. This seems to contradict it:
+//https://softwareengineering.stackexchange.com/questions/118703/where-did-the-notion-of-one-return-only-come-from
+bool is_unique_hash_version(std::string const& input_string)
 {
     std::unordered_set<char> my_set;
-
-    return false;
+    for (char const& i : input_string)
+    {
+        if (my_set.end() == my_set.find(i))
+            my_set.insert(i);
+        else
+            return false;
+    }
+    return true;
 }
 
-void test_unique(std::string const& arg)
+void test_unique(std::string const& input_string)
 {
-    assert(is_unique(arg));
+    assert(is_unique_hash_version      (input_string));
+    assert(is_unique_bool_array        (input_string));
+    assert(is_unique_bitset            (input_string));
+    assert(is_unique_no_data_structures(input_string));
 }
 
-void test_nonunique(std::string const& arg)
+void test_nonunique(std::string const& input_string)
 {
-    assert(!is_unique(arg));
+    assert(!is_unique_hash_version      (input_string));
+    assert(!is_unique_bool_array        (input_string));
+    assert(!is_unique_bitset            (input_string));
+    assert(!is_unique_no_data_structures(input_string));
 }
 
 
 
 void ch1_prob1()
 {
+    std::string unique_test     = "beakmn";
+    std::string non_unique_test = "beakman";
 
-    //newstuffs
-    std::string unique = "beakmn";
-    std::string non_unique = "beakman";
+    test_nonunique(non_unique_test);
+    test_unique   (unique_test);
+
+    std::wstring hello_world = L"Chapter 1 Problem 1 testing passed\n\n";
+    OutputDebugString(hello_world.c_str());
 }
--- CrackingTheCodingInterview/ch1_prob1.h
+++ CrackingTheCodingInterview/ch1_prob1.h
@@ -13,6 +13,12 @@ Solution:
     either 1. hashing to the same key, or 2. having passed load balance, rehash with a bigger
     table. Operation is O(n) but happens at most n/2 times, averaging to O(1))
 
+McDowell's Solution:
+    Part A:
+        Smart, but I'm not sure if it's really 'better'. Implement a bool array of the length of the
+        ASCII character map (if it were all lower-case english aplha non-numerics it would only have
+        to be 26. There are 256 ASCII characters, 128 standard and 128 Extended.
+        
 */
 
 void ch1_prob1();
