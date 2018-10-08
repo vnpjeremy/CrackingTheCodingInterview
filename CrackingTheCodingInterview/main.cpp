8dd32d1e7acfe0479df2dd4b26153a188b0e2cad
Viper-Mobile\KJAKAKJ52 <kreppsjs@gmail.com>
2018-10-07 21:03:20 -0400

    restructure main function for chapters

--- CrackingTheCodingInterview/main.cpp
+++ CrackingTheCodingInterview/main.cpp
@@ -5,14 +5,19 @@
 #include <ch1_prob2.h>
 #include <ch1_prob3.h>
 
+void _chapter1()
+{
+    ch1_prob1();
+    ch1_prob2();
+    ch1_prob3();
+}
+
 int main()
 {
     std::wstring delay = L"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
     OutputDebugString(delay.c_str());
 
-    ch1_prob1();
-    ch1_prob2();
-    ch1_prob3();
+    _chapter1();
 
     std::wstring hello_world = L"\n\nHello, buttmunch\n\n";
     OutputDebugString(hello_world.c_str());
