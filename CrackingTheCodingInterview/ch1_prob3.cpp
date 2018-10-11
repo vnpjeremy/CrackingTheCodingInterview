d5e7ea3aa36b6da097c57e7f253ac7d17fe92e89
Viper-PC\Viper <kreppsjs@gmail.com>
2018-10-10 22:45:45 -0400

    get anagram solution set up for ch1 prob3

--- CrackingTheCodingInterview/ch1_prob3.cpp
+++ CrackingTheCodingInterview/ch1_prob3.cpp
@@ -1,5 +1,35 @@
 #include <ch1_prob3.h>
+#include <string>
+#include <algorithm>
+
+bool is_an_anagram(std::string const& arg1, std::string const& arg2)
+{
+    std::string local_1 = arg1;
+    std::sort(local_1.begin(), local_1.end());
+
+    std::string local_2 = arg2;
+    std::sort(local_2.begin(), local_2.end());
+
+    return local_1 == local_2;
+}
+
+bool is_an_anagram(char* arg1, char* arg2)
+{
+    return false;
+}
+
 void ch1_prob3()
 {
+    std::string const test_string_a = "funeral";
+    std::string const test_string_b = "realfun";
+
+    std::string const test_string_c = "noob";
+    std::string const test_string_d = "boo";
+
+    char const*const test_string_e = "huzzah";
+    char const*const test_string_f = "hazzuh";
+
+    char const*const test_string_g = "nope";
+    char const*const test_string_h = "yup";
 
 }
--- CrackingTheCodingInterview/ch1_prob3.h
+++ CrackingTheCodingInterview/ch1_prob3.h
@@ -1,7 +1,9 @@
 #pragma once
 /*
 Ch 1 Prob 3
+    Given two strings, determine if one is a permutation of the other.
     
+    JSK: This is a classic sort-the-string problem. I won't re-implement a sort here.
 */
 
 void ch1_prob3();
