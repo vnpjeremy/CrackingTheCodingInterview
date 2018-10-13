3424aaa800c0e144d590a27fff211a17fcc1f29c
Viper-Mobile\KJAKAKJ52 <kreppsjs@gmail.com>
2018-10-13 16:23:22 -0400

    Implemented several versions of this solution

--- CrackingTheCodingInterview/ch1_prob3.cpp
+++ CrackingTheCodingInterview/ch1_prob3.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <assert.h>
 #include <print_to_console.h>
+#include <vector>
 
 bool is_an_anagram(std::string const& arg1, std::string const& arg2)
 {
@@ -49,26 +50,56 @@ bool is_an_anagram(char const*const arg1, char const*const arg2)
        unique_ptr is not copyable, but is moveable. It supports
        ctr/dtr and therefore pimpl */
 
+    //WTH you can't use non-static member functions to call std::sort??
+    //we need to use bind or some similar trick to get this even to work?
+    //what a sham! put the damn things in a vector??
     //std::unique_ptr<char> arg1_copy((char*)malloc(arg1_length * sizeof(char))); //std::array?!? //new instead of malloc!?
-    std::unique_ptr<char> arg1_copy(new char[arg1_length]);
+    //std::unique_ptr<char> arg1_copy(new char[arg1_length]);
     //std::unique_ptr<char> arg2_copy((char*)malloc(arg2_length * sizeof(char))); //Default uses 'delete', not free. This will work if you
                                                                                   //do std::unique_ptr<char, free> mm(...);
-    std::unique_ptr<char> arg2_copy(new char[arg2_length]);
+    //std::unique_ptr<char> arg2_copy(new char[arg2_length]);
+    char* arg1_copy(new char[arg1_length]);
+    char* arg2_copy(new char[arg2_length]);
 
-    std::copy(arg1, arg1 + arg1_length, arg1_copy.get());
-    std::copy(arg2, arg2 + arg2_length, arg2_copy.get());
+    std::copy(arg1, arg1 + arg1_length, arg1_copy);
+    std::copy(arg2, arg2 + arg2_length, arg2_copy);
 
-    std::sort(arg1_copy.get(), arg1_copy.get() + arg1_length); //WTH you can't use non-static member functions to call std::sort??
-                                                               //we need to use bind or some similar trick to get this even to work?
-                                                               //what a sham! put the damn things in a vector??
-    std::sort(arg2_copy.get(), arg2_copy.get() + arg2_length);
+    std::sort(arg1_copy, arg1_copy + arg1_length); 
+    std::sort(arg2_copy, arg2_copy + arg2_length);
 
-    bool const is_anagram = std::equal(arg1, arg1 + arg1_length, arg2);
+    bool const is_anagram = std::equal(arg1_copy, arg1_copy + arg1_length, arg2_copy);
 
-    //free(arg1_copy); //I. Hate. Manual. Frees! #RAII
-    //free(arg2_copy);
+    delete[] arg1_copy; //I'd rather just do a vector.
+    delete[] arg2_copy;
     return is_anagram;
 }
+
+bool is_an_anagram_smarter(char const* arg1, char const* arg2)
+{
+    if (!arg1 || !arg2)
+        return false; // or throw, maybe. Shouldn't encounter.
+
+    std::vector<char> arg1_copy;
+    std::vector<char> arg2_copy;
+
+    char const* itr  = arg1;
+    char const* itr2 = arg2;
+    while (*itr)
+        arg1_copy.push_back(*itr++);
+    
+    while (*itr2)
+        arg2_copy.push_back(*itr2++);
+
+    if (arg1_copy.size() != arg2_copy.size())
+        return false;
+
+    std::sort(arg1_copy.begin(), arg1_copy.end());
+    std::sort(arg2_copy.begin(), arg2_copy.end());
+
+    return arg1_copy == arg2_copy;
+    //return std::equal(arg1_copy.begin(), arg1_copy.end(), arg2_copy.begin());
+}
+
 
 void ch1_prob3()
 {
@@ -81,8 +112,6 @@ void ch1_prob3()
     assert(is_an_anagram(test_string_a, test_string_a));
     assert(!is_an_anagram(test_string_c, test_string_d));
     
-    
-
     char const*const test_string_e = "huzzah";
     char const*const test_string_f = "hazzuh";
 
@@ -91,6 +120,9 @@ void ch1_prob3()
 
     assert(is_an_anagram(test_string_e, test_string_f));
     assert(!is_an_anagram(test_string_g, test_string_h));
+
+    assert(is_an_anagram_smarter(test_string_e, test_string_f));
+    assert(!is_an_anagram_smarter(test_string_g, test_string_h));
 
     print_string("Chapter 1 Problem 3 testing passed");
 
--- CrackingTheCodingInterview/ch1_prob3.h
+++ CrackingTheCodingInterview/ch1_prob3.h
@@ -4,6 +4,10 @@ Ch 1 Prob 3
     Given two strings, determine if one is a permutation of the other.
     
     JSK: This is a classic sort-the-string problem. I won't re-implement a sort here.
+
+    Note: If arguments were read-only and we didn't want to pay a copy price, 
+    populate a container, and properly compare, we could declare a [256] and count
+    the number of times each ascii character shows up in the string.
 */
 
 void ch1_prob3();
